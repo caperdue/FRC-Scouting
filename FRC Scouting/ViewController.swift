008ce285edf4bebe794c8142b9e17f1fa0efb975
Carly Perdue <carlyperdue@gmail.com>
2021-05-08 19:35:11 -0400

    Initial Commit

---
+++ FRC Scouting/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FRC Scouting
+//
+//  Created by Carly Perdue on 5/8/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
