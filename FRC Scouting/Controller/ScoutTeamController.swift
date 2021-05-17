c4b18857f441d0e54913fbf1f263eafab886b32c
Carly Perdue <carlyperdue@gmail.com>
2021-05-17 08:04:57 -0400

    added UI changes

--- FRC Scouting/Controller/ScoutTeamController.swift
+++ FRC Scouting/Controller/ScoutTeamController.swift
@@ -9,13 +9,18 @@ import Foundation
 import UIKit
 
 class ScoutTeamController: UIViewController {
+  var images: [UIImage] = []
   
   override func viewDidLoad() {
     super.viewDidLoad()
     
     let rightBarButton = UIBarButtonItem(title: "Save", style: UIBarButtonItem.Style.plain, target: self, action: #selector(self.saveTapped(_:)))
-    
+   
     self.navigationItem.rightBarButtonItem = rightBarButton
+    //TODO: Add horizontal collection view for images.
+    imgCollectionView.dataSource = self
+    imgCollectionView.delegate = self
+    imgCollectionView.alwaysBounceHorizontal = true
     
     
   }
@@ -24,7 +29,7 @@ class ScoutTeamController: UIViewController {
   @IBOutlet weak var teamNameField: UITextField!
   @IBOutlet weak var commentsTextView: UITextView!
   @IBOutlet weak var likeControl: UISegmentedControl!
-  
+  @IBOutlet weak var imgCollectionView: UICollectionView!
   
   @objc func saveTapped(_ sender: UIBarButtonItem) {
     
@@ -44,19 +49,18 @@ class ScoutTeamController: UIViewController {
         print(error.localizedDescription)
         
       }
-      
-      
       navigationController?.popViewController(animated: true)
-      
     }
-    
-    
+  }
+}
+extension ScoutTeamController: UICollectionViewDelegate, UICollectionViewDataSource {
+  func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+    return images.count
   }
   
- 
-
-  
-  
+  func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+    
+  }
   
   
 }
